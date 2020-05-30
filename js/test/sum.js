00864ab30a928e933001058b5bc38a3ff7495bec
Yoichi Izumi <quanyangyi0323@gmail.com>
2020-05-30 06:13:01 +0000

    [mdf] Promise対応

--- js/test/sum.js
+++ js/test/sum.js
@@ -1,21 +1,21 @@
-function sum(filePath, callback) {
-	var xhr = new XMLHttpRequest();
+function sum(filePath) {
+	return new Promise((resolve, reject) => {
+		var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", filePath, true);
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4) {
-			var data = xhr.responseText; // 外部ファイルの内容
+		xhr.open("GET", filePath, true);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				var data = xhr.responseText; // 外部ファイルの内容
 
-			result = Module.ccall(
-				'sumFromText',
-				'string',
-				['string'],
-				[data]);
-
-			callback(result);
-		}
-	};
-	xhr.send();
+				resolve(Module.ccall(
+					'sumFromText',
+					'string',
+					['string'],
+					[data]));
+			}
+		};
+		xhr.send();
+	});
 }
 
 describe('sum 関数のテスト', function () {
@@ -32,18 +32,12 @@ describe('sum 関数のテスト', function () {
 	});
 
 	describe('JavaScript側でファイルを読み込んでからwasm関数を呼び出し', function () {
-		var result = null;
+		it('テキストファイルの内容を与えると合計値とテキストを合わせた文字列を返す', function (done) {
+			sum('./sum_input').then(result => {
+				expect(result).toBe('6 test');
 
-		beforeEach(function (done) {
-			sum('./sum_input', function (res) {
-				result = res;
 				done();
 			});
-		});
-
-		it('テキストファイルの内容を与えると合計値とテキストを合わせた文字列を返す', function (done) {
-			expect(result).toBe('6 test');
-			done();
 		});
 	});
 });
