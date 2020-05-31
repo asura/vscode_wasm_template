bf9b1b5af7d5d7ed935e0e3062ac0eb6bc1fa259
Yoichi Izumi <quanyangyi0323@gmail.com>
2020-05-31 18:31:05 +0000

    [add] IDBFSのテスト

--- cpp/test/Tester.cpp
+++ cpp/test/Tester.cpp
@@ -2,15 +2,31 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 
-TEST_CASE("true")
-{
-    CHECK(1 == 1);
-}
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
+#ifdef EMSCRIPTEN
+EM_JS(void, mountIDBFS, (), {
+    Asyncify.handleSleep(function(wakeUp) {
+        FS.mkdir('/data_idbfs');
+        FS.mount(IDBFS, {}, '/data_idbfs');
+        FS.syncfs(
+            true, function(err) {
+                wakeUp();
+            });
+    });
+});
+#endif
 
 int main(int the_argc, char** the_argv) noexcept
 {
     try
     {
+#ifdef EMSCRIPTEN
+        mountIDBFS();
+#endif
+
         const int result = Catch::Session().run(the_argc, the_argv);
 
         return result;
--- cpp/test/ifstream.cpp
+++ cpp/test/ifstream.cpp
@@ -5,38 +5,78 @@
 #endif
 
 #include <catch2/catch.hpp>
+#include <filesystem>
 #include <fstream>
 
 TEST_CASE("ファイルの読み込み")
 {
 #ifdef EMSCRIPTEN
-    // Emscripten環境だとパス名に "+" があると失敗する
-    const auto base_path = std::string("/data");
-#else
-    const auto base_path = std::string(PROJECT_SOURCE_DIR) + "/../data";
-#endif
-    const auto path = base_path + "/sum_input";
-    INFO("path=" << path);
-
-#ifdef EMSCRIPTEN
-    // ファイルシステム作成
-    EM_ASM_({
-        const path = UTF8ToString($0, $1);
-        FS.mkdir(path);
-        FS.mount(MEMFS, [], path);
-    },
-            base_path.c_str(), base_path.length());
-
-    // ファイル内容を出力
+    SECTION("MEMFS")
     {
-        std::ofstream ofs(path);
-        ofs << "1\n2 3\ntest\n";
+        // Emscripten環境だとパス名に "+" があると失敗する
+        const auto base_path = std::string("/data_memfs");
+        const auto path = base_path + "/sum_input";
+
+        // ファイルシステム作成
+        EM_ASM_({
+            const path = UTF8ToString($0, $1);
+            FS.mkdir(path);
+            FS.mount(MEMFS, [], path);
+        },
+                base_path.c_str(), base_path.length());
+
+        // ファイル内容を出力
+        {
+            std::ofstream ofs(path);
+            ofs << "1\n2 3\ntest\n";
+        }
+
+        std::ifstream ifs(path);
+        REQUIRE(!!ifs);
+
+        const std::string file_contents((std::istreambuf_iterator<char>(ifs)),
+                                        std::istreambuf_iterator<char>());
+
+        CHECK(file_contents == std::string("1\n2 3\ntest\n"));
     }
 
-    // ファイルを書き出し
-    EM_ASM(
-        FS.syncfs(false, function(err){}););
-#endif
+    SECTION("IDBFS")
+    {
+        const auto base_path = std::string("/data_idbfs");
+        const auto path = base_path + "/sum_input";
+
+        if (!std::filesystem::exists(path))
+        {
+            {
+                std::ofstream ofs(path);
+                ofs << "1\n2 3\ntest\n";
+            }
+
+            // ファイルを書き出し
+            EM_ASM(
+                FS.syncfs(
+                    false, function(err) {
+                        if (err)  // error
+                        {
+                            console.error(err);
+                        }
+                    });
+
+            );
+        }
+        REQUIRE(std::filesystem::exists(path));
+
+        std::ifstream ifs(path);
+        REQUIRE(!!ifs);
+
+        const std::string file_contents((std::istreambuf_iterator<char>(ifs)),
+                                        std::istreambuf_iterator<char>());
+
+        CHECK(file_contents == std::string("1\n2 3\ntest\n"));
+    }
+#else
+    const auto base_path = std::string(PROJECT_SOURCE_DIR) + "/../data";
+    const auto path = base_path + "/sum_input";
 
     std::ifstream ifs(path);
     REQUIRE(!!ifs);
@@ -45,4 +85,5 @@ TEST_CASE("ファイルの読み込み")
                                     std::istreambuf_iterator<char>());
 
     CHECK(file_contents == std::string("1\n2 3\ntest\n"));
+#endif
 }
